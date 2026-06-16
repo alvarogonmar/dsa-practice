549aa6ee410dca64c048ab52d13076f235e2e50c
alvarogonmar <alvarogonmar0@gmail.com>
2026-06-16 16:37:33 -0600

    Add implementation of function to check if a string is a palindrome

---
+++ week1/validpalindrome.py
@@ -0,0 +1,17 @@
+s =  "A man, a plan, a canal: Panama"
+
+def palindrome(s):
+        word1 = ""
+        word2 = ""
+        s = s.lower()
+
+        abc = "qwertyuiopasdfghjklzxcvbnm"
+        for letter in s:
+            if letter in abc:
+                word1 += letter
+        for letter in reversed(s):
+            if letter in abc:
+                word2 += letter
+        return word1 == word2
+
+palindrome(s)
